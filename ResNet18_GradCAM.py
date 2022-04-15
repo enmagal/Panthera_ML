5648aa9478e695c027c36900d0b3238dc813254d
Enzo.Magal <Enzo.Magal@L2621427.production.infra>
2022-04-15 11:09:22 +0200

    GradCam Visualization

--- ResNet18_GradCAM.py
+++ ResNet18_GradCAM.py
@@ -1,13 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import transforms, models
 
-from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
-
 from custom_dataset import PantheraDataset
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        
+        # define the resnet152
+        self.resnet = torch.load('./models/Panthera_ResNet18.pt')
+        for p in self.resnet.parameters():
+            p.requires_grad = True
+        
+        # isolate the feature blocks
+        self.features = nn.Sequential(self.resnet.conv1,
+                                      self.resnet.bn1,
+                                      nn.ReLU(),
+                                      nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False),
+                                      self.resnet.layer1, 
+                                      self.resnet.layer2, 
+                                      self.resnet.layer3, 
+                                      self.resnet.layer4)
+        
+        # average pooling layer
+        self.avgpool = self.resnet.avgpool
+        
+        # classifier
+        self.classifier = self.resnet.fc
+        
+        # gradient placeholder
+        self.gradient = None
+    
+    # hook for the gradients
+    def activations_hook(self, grad):
+        self.gradient = grad
+    
+    def get_gradient(self):
+        return self.gradient
+    
+    def get_activations(self, x):
+        return self.features(x)
+    
+    def forward(self, x):
+        
+        # extract the features
+        x = self.features(x)
+        
+        # register the hook
+        h = x.register_hook(self.activations_hook)
+        
+        # complete the forward pass
+        x = self.avgpool(x)
+        x = x.view((1, -1))
+        x = self.classifier(x)
+        
+        return x
 
 if __name__ == '__main__':
     transform = transforms.Compose([transforms.Resize((224, 224)),
@@ -16,17 +69,60 @@ if __name__ == '__main__':
 
     dataset = PantheraDataset('./panthera_dataset/list_photo_prep.csv', './panthera_dataset/img_224/', 'all', transform)    
 
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-    ResNet18 = torch.load('./models/Panthera_ResNet18.pt')
-
-    target_layers = [ResNet18.layer4[-1]]
-    input_tensor = next(iter(dataloader))['image']
-    print(input_tensor.shape)
     
-    cam = GradCAM(model=ResNet18, target_layers=target_layers)
+    # init the resnet
+    resnet = ResNet()
 
-    grayscale_cam = cam(input_tensor=input_tensor, targets=None)
+    # set the evaluation mode
+    _ = resnet.eval()
 
-    grayscale_cam = grayscale_cam[0, :]
-    visualization = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+    # get the image
+    img = next(iter(dataloader))['image']
+
+    # forward pass
+    pred = resnet(img)
+
+    pred.argmax(dim=1) # prints tensor([2])
+
+    # get the gradient of the output with respect to the parameters of the model
+    pred[:,1].backward()
+
+    # pull the gradients out of the model
+    gradients = resnet.get_gradient()
+
+    # pool the gradients across the channels
+    pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+
+    # get the activations of the last convolutional layer
+    activations = resnet.get_activations(img).detach()
+
+    # weight the channels by corresponding gradients
+    for i in range(512):
+        activations[:, i, :, :] *= pooled_gradients[i]
+
+    # average the channels of the activations
+    heatmap = torch.mean(activations, dim=1).squeeze()
+
+    # relu on top of the heatmap
+    # expression (2) in https://arxiv.org/pdf/1610.02391.pdf
+    heatmap = np.maximum(heatmap, 0)
+
+    # normalize the heatmap
+    heatmap /= torch.max(heatmap)
+
+    # draw the heatmap
+    plt.matshow(heatmap.squeeze())
+
+    # make the heatmap to be a numpy array
+    heatmap = heatmap.numpy()
+
+    img = cv2.imread('./panthera_dataset/img/12310285.jpg')
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = heatmap * 0.4 + img
+    cv2.imwrite('./Results/GradCAM/map.jpg', superimposed_img)
+
+    
--- model_validation.py
+++ model_validation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 from PIL import Image
 
 import torch
@@ -6,6 +9,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms, models
 
 from custom_dataset import PantheraDataset
+from ResNet18_GradCAM import ResNet
 
 if __name__ == '__main__':
 
@@ -25,7 +29,6 @@ if __name__ == '__main__':
     dfTest['Photo'] = dfTest['Photo'].astype(str)
 
     ResNet18 = torch.load('./models/Panthera_ResNet18.pt')
-    print(ResNet18.layer4[-1])
 
     for batch in dataloader:
         x = batch['image'].to(device)
@@ -34,12 +37,65 @@ if __name__ == '__main__':
         
     dfTest['ResNet18_succeed'] = (torch.max(pred, 1)[1] == y).numpy()
 
-    dfTest = dfTest.loc[dfTest['ResNet18_succeed'] == False]
+    dfTest = dfTest.loc[dfTest['ResNet18_succeed'] == True]
     dfTest = dfTest.reset_index(drop=True)
+
+    dfTest.to_csv('./panthera_dataset/bad_prediction.csv', index=False)
 
     root_dir = './panthera_dataset/img/'
 
-    for i in range(len(dfTest)):
-        image_path = root_dir + str(dfTest.loc[i, 'Photo']) + '.jpg'
-        image = Image.open(image_path)
-        image.show()
+    dataset = PantheraDataset('./panthera_dataset/bad_prediction.csv', './panthera_dataset/img_224/', 'all', transform)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=0)
+
+    resnet = ResNet()
+
+    idx = 0
+    for batch in dataloader:
+        # get the image
+        img = batch['image']
+
+        # forward pass
+        pred = resnet(img)
+
+        pred.argmax(dim=1) # prints tensor([2])
+
+        # get the gradient of the output with respect to the parameters of the model
+        pred[:,1].backward()
+
+        # pull the gradients out of the model
+        gradients = resnet.get_gradient()
+
+        # pool the gradients across the channels
+        pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+
+        # get the activations of the last convolutional layer
+        activations = resnet.get_activations(img).detach()
+
+        # weight the channels by corresponding gradients
+        for i in range(512):
+            activations[:, i, :, :] *= pooled_gradients[i]
+
+        # average the channels of the activations
+        heatmap = torch.mean(activations, dim=1).squeeze()
+
+        # relu on top of the heatmap
+        # expression (2) in https://arxiv.org/pdf/1610.02391.pdf
+        heatmap = np.maximum(heatmap, 0)
+
+        # normalize the heatmap
+        heatmap /= torch.max(heatmap)
+
+        # draw the heatmap
+        plt.matshow(heatmap.squeeze())
+
+        # make the heatmap to be a numpy array
+        heatmap = heatmap.numpy()
+
+        img = cv2.imread('./panthera_dataset/img/' + dfTest.loc[idx, 'Photo'] + '.jpg')
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+        heatmap = np.uint8(255 * heatmap)
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        superimposed_img = heatmap * 0.4 + img
+        cv2.imwrite('./Results/GradCAM/' + dfTest.loc[idx, 'Photo'] + '.jpg', superimposed_img)
+
+        idx += 1
